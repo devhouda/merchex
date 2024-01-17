fc1480a0b65ace38666cb9fdefa914ed7bdd37a8
DevHouda <devhouda@gmail.com>
2024-01-17 10:51:00 +0100

    read data in list view and detail view

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,16 +4,25 @@ from listings.models import Band
 from listings.models import Listing
 
 # Create your views here.
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html', context={'bands': bands})
+    return render(request, 'listings/band_list.html', context={'bands': bands})
+
+def band_detail(request, id):
+    band = Band.objects.get(id=id)
+    return render(request, 'listings/band_detail.html', context={'band': band})
 
 def about(request):
     return HttpResponse('<h1>About Us</h1> <p>We love merch!</p>')
 
-def listings(request):
+def listing_list(request):
     listings = Listing.objects.all()
-    return render(request, 'listings/listings.html', context={'listings': listings})
+    return render(request, 'listings/listing_list.html', context={'listings': listings})
+
+def listing_detail(request, id):
+    listing = Listing.objects.get(id=id)
+    return render(request, 'listings/listing_detail.html', context={'listing': listing})
+
 
 def contact(request):
     return HttpResponse('<h1>Contact Us</h1> <p>55555555</p>')
