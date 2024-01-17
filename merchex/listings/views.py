d103b2b159dd85b96c3dbd16aa6442c3c47e509f
DevHouda <devhouda@gmail.com>
2024-01-17 16:16:28 +0100

    django forms

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from listings.models import Band
 from listings.models import Listing
+from listings.forms import ContactUsForm, BandForm, ListingForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def band_list(request):
@@ -12,8 +14,18 @@ def band_detail(request, id):
     band = Band.objects.get(id=id)
     return render(request, 'listings/band_detail.html', context={'band': band})
 
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm()
+    return render(request, 'listings/band_create.html', {'form': form})
+
 def about(request):
-    return HttpResponse('<h1>About Us</h1> <p>We love merch!</p>')
+    return render(request, 'listings/about.html')
 
 def listing_list(request):
     listings = Listing.objects.all()
@@ -23,6 +35,27 @@ def listing_detail(request, id):
     listing = Listing.objects.get(id=id)
     return render(request, 'listings/listing_detail.html', context={'listing': listing})
 
+def listing_create(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = BandForm()
+    return render(request, 'listings/listing_create.html', {'form': form})
+
+
 
 def contact(request):
-    return HttpResponse('<h1>Contact Us</h1> <p>55555555</p>')
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            send_mail(subject=f'Message from {form.cleaned_data["name"] or "anonymous"} via MerchEx contact us form', 
+                      message=form.cleaned_data["message"],
+                      from_email=form.cleaned_data["email"],
+                      recipient_list=['admin@marchex.xyz'])
+            return redirect('email-sent')
+    else:
+        form = ContactUsForm()
+    return render(request, 'listings/contact.html', context={'form': form})
